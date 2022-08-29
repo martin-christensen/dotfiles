d776cb6cb4d1d820a777a746125a824efd3afdf0
Martin Hjorth Christensen <martin@diwe.dk>
2022-08-29 12:41:02 +0200

    Added OpenVPN to brew cask. Added slack to mac dock.

--- macos/dock.sh
+++ macos/dock.sh
@@ -4,6 +4,7 @@ dockutil --no-restart --remove all
 dockutil --no-restart --add "/System/Applications/Mail.app"
 dockutil --no-restart --add "/System/Applications/Calendar.app"
 dockutil --no-restart --add "/System/Applications/Messages.app"
+dockutil --no-restart --add "/Applications/Slack.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
 dockutil --no-restart --add "/System/Applications/Reminders.app"
 dockutil --no-restart --add "/System/Applications/Notes.app"
