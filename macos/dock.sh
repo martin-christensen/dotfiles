6ee17aee386cfc316680bc874c42822fae4522ae
Martin Hjorth Christensen <martin@diwe.dk>
2022-09-07 12:25:04 +0200

    Added firefox and cleaned up my dock.

--- macos/dock.sh
+++ macos/dock.sh
@@ -3,30 +3,29 @@
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/System/Applications/Mail.app"
 dockutil --no-restart --add "/System/Applications/Calendar.app"
-dockutil --add '' --type small-spacer --section apps --after Calendar
 dockutil --no-restart --add "/System/Applications/Messages.app"
 dockutil --no-restart --add "/Applications/Slack.app"
 dockutil --no-restart --add "/Applications/Google Meet.app"
-dockutil --add '' --type small-spacer --section apps --after "Google Meet"
+dockutil --no-restart --add "/Applications/Microsoft Teams.app"
+dockutil --no-restart --add '' --type small-spacer --section apps
 dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --add '' --type small-spacer --section apps --after "Google Chrome"
-dockutil --no-restart --add "/System/Applications/Reminders.app"
-dockutil --no-restart --add "/Applications/Evernote.app"
-dockutil --no-restart --add "/System/Applications/Notes.app"
-dockutil --add '' --type small-spacer --section apps --after Notes
+dockutil --no-restart --add "/Applications/Firefox.app"
 dockutil --no-restart --add "/Applications/VSCodium.app"
 dockutil --no-restart --add "/Applications/Sourcetree.app"
 dockutil --no-restart --add "/Applications/Sequel Ace.app"
-dockutil --no-restart --add "/Applications/Docker.app"
+dockutil --no-restart --add "/Applications/Docker.app/Contents/MacOS/Docker Desktop.app"
 dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
 dockutil --no-restart --add "/Applications/Postman.app"
-dockutil --add '' --type small-spacer --section apps --after Postman
-dockutil --no-restart --add "/System/Applications/App Store.app"
-dockutil --no-restart --add "/System/Applications/System Preferences.app"
-dockutil --add '' --type small-spacer --section apps --after "System Preferences"
+dockutil --no-restart --add '' --type small-spacer --section apps
+dockutil --no-restart --add "/System/Applications/Reminders.app"
+dockutil --no-restart --add "/Applications/Evernote.app"
+dockutil --no-restart --add "/System/Applications/Notes.app"
+dockutil --no-restart --add '' --type small-spacer --section apps
 dockutil --no-restart --add "/Applications/Toggl Track.app"
-dockutil --add '' --type small-spacer --section apps --after "Toggl Track"
 dockutil --no-restart --add "/Applications/Spotify.app"
 dockutil --no-restart --add "/Applications/Google Photos.app"
+dockutil --no-restart --add "/System/Applications/App Store.app"
+dockutil --no-restart --add "/System/Applications/System Preferences.app"
+dockutil --no-restart --add "~/Downloads" --view grid --display folder --sort dateadded --section others
 
 killall Dock
