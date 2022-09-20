7867e0e62d690b67ff6d55a7a6dbc9d025896907
Martin Hjorth Christensen <martin@diwe.dk>
2022-09-20 15:48:05 +0200

    Added Facebook Messenger.

--- macos/dock.sh
+++ macos/dock.sh
@@ -4,6 +4,7 @@ dockutil --no-restart --remove all
 dockutil --no-restart --add "/System/Applications/Mail.app"
 dockutil --no-restart --add "/System/Applications/Calendar.app"
 dockutil --no-restart --add "/System/Applications/Messages.app"
+dockutil --no-restart --add "/Applications/Messenger.app"
 dockutil --no-restart --add "/Applications/Slack.app"
 dockutil --no-restart --add "/Applications/Google Meet.app"
 dockutil --no-restart --add "/Applications/Microsoft Teams.app"
